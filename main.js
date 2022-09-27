37b4a49d12c6392838759d5ec51d96e5ce19577b
albtony <albtony21@gmail.com>
2022-09-27 14:29:40 +0700

    complete assignment

---
+++ main.js
@@ -0,0 +1,43 @@
+window.submitf = () => {
+    const getValue = function(id) {
+        const elem = document.getElementById(id);
+        if(elem.value) return elem.value
+        else return null;
+    };
+
+    const peserta = {
+        nama: getValue("nama"),
+        instansi: getValue("instansi"),
+        alamat: getValue("alamat"),
+        kodePos: getValue("kodepos"),
+        noTelp: getValue("noTelp"),
+        email: getValue("email"),
+        sponsor: getValue("sponsor")
+    }
+
+    if(isInputBlank(peserta) || isInputBlank(perusahaan)) 
+        return;
+
+    if(!isPhoneNumber(peserta.noTelp) || !isPhoneNumber(perusahaan.noTelp))
+        return;
+
+    alert('Form Submitted!');
+}
+
+window.isPhoneNumber = (number) => {
+    if(/\+?([ -]?\d+)+|\(\d+\)([ -]\d+)/.test(number)) {
+        alert(`Please recheck your phone number`);
+        return true;
+    }
+    return false;
+}
+
+window.isInputBlank = (object) => {
+    for (const field in object) {
+        if(object.field == null) {
+            alert(`Please fill out the blank`);
+            return true;
+        } 
+        return false;
+    }
+}
